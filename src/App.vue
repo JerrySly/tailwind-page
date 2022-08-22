5fc2dad9473e59344c6eabdec032cdc979426439
JerrySly <eugeale@yandex.ru>
2022-08-22 18:25:54 +0300

    init (game-accordion)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,247 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="games-accordion" class="flex">
+    <div
+      class="
+        h-[800px]
+        ease-in-out
+        duration-300
+        bg-center bg-top
+        min-w-[2.05rem]
+        game-1
+        flex-1
+        cursor-pointer
+        hover:flex-[9]
+        group
+        relative
+      "
+    >
+      <div
+        class="
+          absolute
+          bg-yellow-400
+          left-0
+          w-12
+          group-hover:opacity-0
+          h-full
+          ease-in-out
+          duration-300
+        "
+      >
+        <div class="-rotate-90 mt-[700px]">
+          <span class="text-white font-bold text-3xl font-silk-screen"
+            >Crysis</span
+          >
+        </div>
+      </div>
+      <div
+        class="
+          opacity-0
+          absolute
+          group-hover:opacity-100
+          ease-in-out
+          duration-300
+          bottom-0
+          w-full
+          bg-gradient-to-t
+          from-gray-500
+          to-gray-50/[.01]
+        "
+      >
+        <h2 class="text-white pl-5 mb-2 text-3xl">Crysis</h2>
+        <div class="text-white p-5 text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+          tellus lacus. Phasellus vel arcu vehicula, ornare dolor pellentesque,
+          sollicitudin urna. Maecenas eleifend malesuada commodo. Nullam blandit
+          aliquet risus, eget faucibus est aliquet quis. Aliquam at ante
+          vehicula, ultrices diam quis, semper ante. Nam lacinia diam id ex
+          dapibus semper. In dictum, elit at luctus hendrerit, quam ante dapibus
+          libero, ut efficitur elit sem id enim. Nulla facilisi. Aliquam eget
+          felis vestibulum,
+        </div>
+      </div>
+    </div>
+    <div
+      class="
+        h-[800px]
+        ease-in-out
+        duration-300
+        bg-center bg-top
+        min-w-[2.05rem]
+        game-2
+        flex-1
+        cursor-pointer
+        hover:flex-[9]
+        group
+        relative
+      "
+    >
+      <div
+        class="absolute bg-yellow-400 left-0 w-12 h-full group-hover:opacity-0"
+      >
+        <div class="-rotate-90 mt-[700px]">
+          <span class="text-white font-bold text-3xl font-silk-screen"
+            >Battlefield</span
+          >
+        </div>
+      </div>
+      <div
+        class="
+          opacity-0
+          absolute
+          group-hover:opacity-100
+          ease-in-out
+          duration-300
+          bottom-0
+          w-full
+          bg-gradient-to-t
+          from-gray-500
+          to-gray-50/[.01]
+        "
+      >
+        <h2 class="text-white pl-5 mb-2 text-3xl">Crysis</h2>
+        <div class="text-white p-5 text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+          tellus lacus. Phasellus vel arcu vehicula, ornare dolor pellentesque,
+          sollicitudin urna. Maecenas eleifend malesuada commodo. Nullam blandit
+          aliquet risus, eget faucibus est aliquet quis. Aliquam at ante
+          vehicula, ultrices diam quis, semper ante. Nam lacinia diam id ex
+          dapibus semper. In dictum, elit at luctus hendrerit, quam ante dapibus
+          libero, ut efficitur elit sem id enim. Nulla facilisi. Aliquam eget
+          felis vestibulum,
+        </div>
+      </div>
+    </div>
+    <div
+      class="
+        h-[800px]
+        ease-in-out
+        duration-300
+        bg-center bg-top
+        min-w-[2.05rem]
+        game-3
+        flex-1
+        cursor-pointer
+        hover:flex-[9]
+        group
+        relative
+      "
+    >
+      <div
+        class="absolute bg-yellow-400 left-0 w-12 h-full group-hover:opacity-0"
+      >
+        <div class="-rotate-90 mt-[700px] w-full">
+          <span
+            class="
+              text-white
+              font-bold
+              text-3xl
+              font-silk-screen
+              whitespace-nowrap
+            "
+            >Dark Souls</span
+          >
+        </div>
+      </div>
+      <div
+        class="
+          opacity-0
+          absolute
+          group-hover:opacity-100
+          ease-in-out
+          duration-300
+          bottom-0
+          w-full
+          bg-gradient-to-t
+          from-gray-500
+          to-gray-50/[.01]
+        "
+      >
+        <h2 class="text-white pl-5 mb-2 text-3xl">Crysis</h2>
+        <div class="text-white p-5 text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+          tellus lacus. Phasellus vel arcu vehicula, ornare dolor pellentesque,
+          sollicitudin urna. Maecenas eleifend malesuada commodo. Nullam blandit
+          aliquet risus, eget faucibus est aliquet quis. Aliquam at ante
+          vehicula, ultrices diam quis, semper ante. Nam lacinia diam id ex
+          dapibus semper. In dictum, elit at luctus hendrerit, quam ante dapibus
+          libero, ut efficitur elit sem id enim. Nulla facilisi. Aliquam eget
+          felis vestibulum,
+        </div>
+      </div>
+    </div>
+    <div
+      class="
+        h-[800px]
+        ease-in-out
+        duration-300
+        bg-center bg-top
+        min-w-[2.05rem]
+        game-4
+        flex-1
+        cursor-pointer
+        hover:flex-[9]
+        group
+        relative
+      "
+    >
+      <div
+        class="absolute bg-yellow-400 left-0 w-12 h-full group-hover:opacity-0"
+      >
+        <div class="-rotate-90 mt-[700px]">
+          <span class="text-white font-bold text-3xl font-silk-screen"
+            >Forza</span
+          >
+        </div>
+      </div>
+      <div
+        class="
+          opacity-0
+          absolute
+          group-hover:opacity-100
+          ease-in-out
+          duration-300
+          bottom-0
+          w-full
+          bg-gradient-to-t
+          from-gray-500
+          to-gray-50/[.01]
+        "
+      >
+        <h2 class="text-white pl-5 mb-2 text-3xl">Crysis</h2>
+        <div class="text-white p-5 text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+          tellus lacus. Phasellus vel arcu vehicula, ornare dolor pellentesque,
+          sollicitudin urna. Maecenas eleifend malesuada commodo. Nullam blandit
+          aliquet risus, eget faucibus est aliquet quis. Aliquam at ante
+          vehicula, ultrices diam quis, semper ante. Nam lacinia diam id ex
+          dapibus semper. In dictum, elit at luctus hendrerit, quam ante dapibus
+          libero, ut efficitur elit sem id enim. Nulla facilisi. Aliquam eget
+          felis vestibulum,
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="promo">
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.game-1 {
+  background-image: url("./assets/imgs/crysis.jpg");
+}
+.game-2 {
+  background-image: url("./assets/imgs/battlefield.jpeg");
+}
+.game-3 {
+  background-image: url("./assets/imgs/dark-souls.jpg");
+}
+.game-4 {
+  background-image: url("./assets/imgs/forza.jpeg");
 }
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      "silk-screen": ["Silkscreen", "cursive"],
+    },
+  },
+  plugins: [],
+};
