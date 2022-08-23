d36f0734893633ea50299535314b0a80fe8f7292
JerrySly <eugeale@yandex.ru>
2022-08-23 22:25:12 +0300

    add new blocks (drinks,form,footer)

--- src/App.vue
+++ src/App.vue
@@ -221,13 +221,222 @@
       </div>
     </div>
   </div>
-  <div id="promo">
-    
+  <div class="bg-gradient-to-t from-yellow-400 to-white h-32 w-full"></div>
+  <div id="promo" class="pb-12 bg-gradient-to-t from-orange-500 to-yellow-400">
+    <div class="text-white text-center font-silk-screen text-5xl">GameFuel</div>
+    <div
+      class="text-white text-center  text-2xl mt-12 pl-12 pr-12"
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+      tellus lacus. Phasellus vel arcu vehicula, ornare dolor pellentesque,
+      sollicitudin urna. Maecenas eleifend malesuada commodo. Nullam blandit
+      aliquet risus, eget faucibus est aliquet quis. Aliquam at ante vehicula,
+      ultrices diam quis, semper ante.
+    </div>
+  </div>
+  <div id="locations" class="bg-orange-500 w-full pt-12">
+    <div class="font-silk-screen text-5xl text-center text-white">
+      Our cities
+    </div>
+    <div class="flex justify-between pl-28 pr-28 mt-16">
+      <div
+        class="
+          group
+          text-center
+          p-3
+          rounded-lg
+          border-solid border-2
+          cursor-pointer
+        "
+      >
+        <div
+          class="
+            bg-white
+            w-80
+            h-80
+            flex
+            justify-center
+            items-center
+            rounded-full
+          "
+        >
+          <city-icon width="200px" height="200px" />
+        </div>
+        <div class="font-silk-screen text-4xl text-white mt-3">Kazan</div>
+        <div
+          class="
+            ease-in-out
+            duration-300
+            opacity-0
+            group-hover:opacity-100
+            text-white text-2xl
+          "
+        >
+          street Chehova, house 64
+        </div>
+      </div>
+      <div
+        class="
+          group
+          text-center
+          p-3
+          rounded-lg
+          border-solid border-2
+          cursor-pointer
+        "
+      >
+        <div
+          class="
+            bg-white
+            w-80
+            h-80
+            flex
+            justify-center
+            items-center
+            rounded-full
+          "
+        >
+          <city-icon width="200px" height="200px" />
+        </div>
+        <div class="font-silk-screen text-4xl text-white mt-3">Moscow</div>
+        <div
+          class="
+            ease-in-out
+            duration-300
+            opacity-0
+            group-hover:opacity-100
+            text-white text-2xl
+          "
+        >
+          street Chehova, house 64
+        </div>
+      </div>
+      <div
+        class="
+          group
+          text-center
+          p-3
+          rounded-lg
+          border-solid border-2
+          cursor-pointer
+        "
+      >
+        <div
+          class="
+            bg-white
+            w-80
+            h-80
+            flex
+            justify-center
+            items-center
+            rounded-full
+          "
+        >
+          <city-icon width="200px" height="200px" />
+        </div>
+        <div class="font-silk-screen text-4xl text-white mt-3">Ufa</div>
+        <div
+          class="
+            ease-in-out
+            duration-300
+            opacity-0
+            group-hover:opacity-100
+            text-white text-2xl
+          "
+        >
+          street Chehova, house 64
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bg-gradient-to-b from-orange-500 to-white h-10"></div>
+  <div id="drinks-info" class="pt-20">
+    <div class="flex bg-white pl-28 pr-28">
+      <div class="flex-2 m-auto">
+        <div class="text-orange-500 text-3xl font-silk-screen mb-10">Red Bull</div>
+        <div class="text-orange-500 text-2xl ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+      tellus lacus. Phasellus vel arcu vehicula, ornare dolor pellentesque,
+      sollicitudin urna. Maecenas eleifend malesuada commodo. Nullam blandit
+      aliquet risus, eget faucibus est aliquet quis. Aliquam at ante vehicula,
+      ultrices diam quis, semper ante.
+        </div>
+      </div>
+      <div class="flex-1">
+        <img class="h-[500px] w-[600px] max-w-none" src="./assets/imgs/drinks/red-bull.png" />
+      </div>
+    </div>
+    <div class="flex bg-orange-500">
+      <div class="flex-1">
+        <img class="h-[500px] w-[500px] max-w-none" src="./assets/imgs/drinks/burn.png" />
+      </div>
+      <div class="flex-2 m-auto">
+        <div class="text-white text-3xl font-silk-screen mb-10">Burn</div>
+        <div class="text-white text-2xl ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+      tellus lacus. Phasellus vel arcu vehicula, ornare dolor pellentesque,
+      sollicitudin urna. Maecenas eleifend malesuada commodo. Nullam blandit
+      aliquet risus, eget faucibus est aliquet quis. Aliquam at ante vehicula,
+      ultrices diam quis, semper ante.
+        </div>
+      </div>
+    </div>
+    <div class="flex bg-white pl-28 pr-28">
+      <div class="flex-2 m-auto ">
+        <div class="text-orange-500 text-3xl font-silk-screen mb-10">Monster</div>
+        <div class="text-orange-500 text-2xl ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+      tellus lacus. Phasellus vel arcu vehicula, ornare dolor pellentesque,
+      sollicitudin urna. Maecenas eleifend malesuada commodo. Nullam blandit
+      aliquet risus, eget faucibus est aliquet quis. Aliquam at ante vehicula,
+      ultrices diam quis, semper ante.
+        </div>
+      </div>
+      <div class="flex-1 ml-10">
+        <img class="h-[500px] w-[300px] max-w-none" src="./assets/imgs/drinks/monster.png" />
+      </div>
+    </div>
+  </div>
+  <div class="bg-gradient-to-t from-orange-500 to-white h-10"></div>
+  <div class="pt-10 bg-orange-500 text-white text-5xl text-center font-silk-screen pb-8">
+      Connect with us
+  </div>
+  <div id="form" class="pb-10 flex justify-center items-center  bg-orange-500 ">
+    <div class="border-2 border-white rounded-lg p-10">
+      <div class="font-silk-screen text-center text-white text-2xl">
+        Send your email and we can connect with you =)
+      </div>
+      <div class="text-center mt-10">
+      <input placeholder="email" type="email" class="w-[400px] pl-3 font-silk-screen rounded-xl h-12 outline-0">
+      </div>
+      <div class="text-center mt-10">
+      <button type="submit" class="bg-white rounded-md font-silk-screen w-40 text-2xl text-orange-800 hover:text-white hover:bg-orange-500 easy-in-out duration-300">Send</button>
+      </div>
+    </div>
+  </div>
+  <div class="bg-gradient-to-b from-orange-500 via-yellow-400 to-white h-28"></div>
+  <div id="footer" class="flex bg-black text-white justify-between pl-28 pr-28 pt-10 pb-10">
+    <div>
+      <div class="font-silk-screen text-3xl">Our contacts:</div>
+      <div class="font-silk-screen text-xl">Email: eugeale@yandex.ru</div>
+      <div class="font-silk-screen text-xl">Phone: 7-XXX-XXX-XX-XX</div>
+    </div>
+    <div>
+      <div class="font-silk-screen text-2xl">Our social media:</div>
+      <div class="mt-2">
+      <vk-icon class="cursor-pointer inline-block" width="48px" height="48px"/>
+      <telegram-icon class="cursor-pointer inline-block ml-5" width="40px" height="40px"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import cityIcon from "./components/cityIcon.vue";
+import TelegramIcon from './components/telegramIcon.vue';
+import VkIcon from './components/vkIcon.vue';
 export default {
+  components: { cityIcon, VkIcon,TelegramIcon },
   name: "App",
 };
 </script>
